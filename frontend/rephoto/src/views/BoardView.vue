0155a0d8a706cf75622cb268baf8769b35532cc1
KimMingyeong <kimmg0708@gmail.com>
2024-11-26 13:09:09 +0900

    Design Board, index 완성

--- frontend/rephoto/src/views/BoardView.vue
+++ frontend/rephoto/src/views/BoardView.vue
@@ -1,20 +1,35 @@
 <template>
   <div>
-    <RouterLink :to="`/board`" id="title"><h2 class="text-center">REPHOTO : GALLERY</h2></RouterLink>
-    <RouterView/>
+    <RouterLink :to="`/board`" id="title" class="custom-font">
+      <h2 class="text-center">REPHOTO : GALLERY</h2>
+    </RouterLink>
+    <RouterView />
   </div>
 </template>
 
 <script setup>
-
 import { RouterView } from 'vue-router';
-
 </script>
 
 <style scoped>
-#title{
+/* @font-face 정의 */
+@font-face {
+    font-family: 'Hahmlet-Regular';
+    src: url('https://fastly.jsdelivr.net/gh/projectnoonnu/noonfonts_2110@1.0/Hahmlet-Regular.woff2') format('woff2');
+    font-weight: normal;
+    font-style: normal;
+}
+
+/* 커스텀 폰트 스타일 */
+.custom-font h2 {
+  font-family: 'Hahmlet-Regular', sans-serif; /* 적용할 폰트 */
+  font-size: 28px; /* 글씨 크기 */
+  color: #fff; /* 텍스트 색상 */
+  text-shadow: 1px 1px 3px rgba(0, 0, 0, 0.5); /* 텍스트 강조 효과 */
+}
+
+#title {
   text-decoration: none;
   color: #fff;
 }
-
 </style>
